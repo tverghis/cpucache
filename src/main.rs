c5927c80b1005d4829a09de590cef4ff542c726d
Tarun Verghis <tarun.verghis@gmail.com>
2025-01-09 02:52:12 -0800

    Compare array access using stride widths 1, 16

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    let strides = [1, 16];
+
+    for stride in strides {
+        let mut array = vec![1i32; 64 * 1024 * 1024];
+
+        let start = Instant::now();
+        workload(&mut array, stride);
+        let dur = start.elapsed().as_millis();
+
+        println!("(stride={stride}) duration: {}ms", dur);
+    }
+}
+
+fn workload(arr: &mut [i32], stride: usize) {
+    for i in (0..arr.len()).step_by(stride) {
+        arr[i] *= 3;
+    }
 }
