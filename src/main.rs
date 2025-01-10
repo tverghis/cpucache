458636019abd3a9807a4701f6c111f1e42e19fd6
Tarun Verghis <tarun.verghis@gmail.com>
2025-01-10 00:32:45 -0800

    Initialize vector with ones

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let strides = [1, 16];
 
     for stride in strides {
-        let mut array = vec![0i32; 64 * 1024 * 1024];
+        let mut array = vec![1i32; 64 * 1024 * 1024];
         let start = Instant::now();
         workload(&mut array, stride);
         let dur = start.elapsed().as_millis();
